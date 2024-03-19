a30b54972c7b5e004286cfb4a6f56678f01c4fee
tr-wang <17795194396@163.com>
2024-03-19 18:19:41 +0800

    feature(custom): 应用左侧栏样式按照ui还原，以及选中效果，菜单选中导航栏联动功能

--- src/renderer/src/layout/menu/index.vue
+++ src/renderer/src/layout/menu/index.vue
@@ -1,29 +1,51 @@
 <template>
   <el-menu
-    default-active="2"
     class="el-menu-vertical-demo"
     :collapse="isCollapse"
     @open="handleOpen"
     @close="handleClose"
+    @select="selectMenu"
   >
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
+    <div class="profile_picture">
+      <img
+        src="C:\Users\17795\Desktop\intelligent-marketing-ai\src\renderer\src\assets\icons\image.png"
+        style="width: 48px; height: 48px; border-radius: 24px;"
+      >
+      <div class="tip"></div>
+    </div>
+    <el-menu-item index="1">
+      <div :class="{'blue_box': true, 'active_blue': changeColorIndex === 1}">
+        <svg-icon
+          name="console"
+          :icon-style="{ width: 32, height: 32 }"
+        />
+      </div>
       <template #title>
-        Navigator Two
+        <span :class="{'menu_font': true, 'active_font': changeColorIndex === 1}">控制台</span>
       </template>
     </el-menu-item>
     <el-menu-item
-      index="3"
+      index="2"
     >
-      <el-icon><document /></el-icon>
+      <div :class="{'blue_box': true, 'active_blue': changeColorIndex === 2}">
+        <svg-icon
+          name="myAccount"
+          :icon-style="{ width: 32, height: 32 }"
+        />
+      </div>
       <template #title>
-        Navigator Three
+        <span :class="{'menu_font': true, 'active_font': changeColorIndex === 2}">我的账号</span>
       </template>
     </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon><setting /></el-icon>
+    <el-menu-item index="3">
+      <div :class="{'blue_box': true, 'active_blue': changeColorIndex === 3}">
+        <svg-icon
+          name="groupHairAssistant"
+          :icon-style="{ width: 32, height: 32 }"
+        />
+      </div>
       <template #title>
-        Navigator Four
+        <span :class="{'menu_font': true, 'active_font': changeColorIndex === 3}">群发助手</span>
       </template>
     </el-menu-item>
   </el-menu>
@@ -37,15 +59,22 @@ import {
   Menu as IconMenu,
   Setting,
 } from '@element-plus/icons-vue'
+// import { ref } from 'vue';
 // const isCollapse = ref(true)
 const store = useMenuStore()
 
-const { isCollapse } = storeToRefs(store)
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+// const changeColorIndex = ref(1)
+
+const { isCollapse, changeColorIndex } = storeToRefs(store)
+// const handleOpen = (key: string, keyPath: string[]) => {
+//   console.log(key, keyPath)
+// }
+// const handleClose = (key: string, keyPath: string[]) => {
+//   console.log(key, keyPath)
+// }
+
+const selectMenu = (index: number) => {
+  store.setChangeColorIndex(index)
 }
 </script>
 
@@ -55,8 +84,72 @@ const handleClose = (key: string, keyPath: string[]) => {
   height: 100%;
   background: linear-gradient(180deg, #81affe 0%, #5691fe 16%, #1b68ff 100%);
 }
+.el-menu--collapse {
+    width: 84px;
+    background: linear-gradient(180deg, #81AFFE 0%, #5691FE 16%, #1B68FF 100%);
+    opacity: 1;
+}
+// .el-menu--collapse.is-hover {
+//   background-color: none;
+// }
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+  opacity: 1;
+  background:  linear-gradient(180deg, #81AFFE 0%, #5691FE 16%, #1B68FF 100%);
 }
+.el-menu-item:hover {
+  background-color: transparent !important;
+}
+.el-menu-item .el-menu-tooltip__trigger {
+  padding: 0 !important;
+  // position: none !important;
+  // left: 27px !important;
+    left:50%;
+    margin-left:-25px;/*宽度的一半*/
+}
+.profile_picture {
+  width: 52px;
+  height: 52px;
+  margin: 18px 0 20px 20px;
+  position: relative;
+  .tip {
+    position: absolute;
+    right: 4px;
+    bottom: 2px;
+    width: 16px;
+    height: 16px;
+    background: #00BF80;
+    border-radius: 50px 50px 50px 50px;
+    opacity: 1;
+  }
+}
+.blue_box {
+  width: 52px;
+  height: 52px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 16px;
+}
+.active_blue {
+  opacity: 1;
+  background: #1966FF;
+  border-radius: 10px 10px 10px 10px;
+}
+
+.menu_font {
+  font-size: 16px;
+  font-family: PingFang SC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #FFFFFF;
+  line-height: 22px;
+}
+
+.active_font {
+  font-size: 18px;
+  font-weight: 800;
+  line-height: 22px;
+}
+
 </style>
