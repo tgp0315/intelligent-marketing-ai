1fc2d38d62f9db7794ad6a28b3e39220367b4df6
tr-wang <17795194396@163.com>
2024-03-20 10:34:56 +0800

    feature(custom): 添加页面路由，实现点击菜单进行切换

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -10,7 +10,23 @@ const routes: RouteRecordRaw[] = [
     name: 'home',
     // redirect: '',
     component: () => import('@/layout/index.vue'),
-    children: []
+    children: [
+      {
+        path: '/staging',
+        name: 'staging',
+        component: () => import('@/views/staging/index.vue')
+      },
+      {
+        path: '/myAccount',
+        name: 'myAccount',
+        component: () => import('@/views/myAccount/index.vue')
+      },
+      {
+        path: '/groupHairAssistant',
+        name: 'groupHairAssistant',
+        component: () => import('@/views/groupHairAssistant/index.vue')
+      }
+    ]
   },
   {
     path: '/login',
---
+++ src/renderer/src/views/staging/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    工作台
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
