af0154baa5758d98280a32c04c8e1bdb03a3fc78
tr-wang <17795194396@163.com>
2024-03-22 18:51:54 +0800

    feature(custom): 群发助手内的群发列表页面排版

---
+++ src/renderer/src/views/groupHairAssistant/components/taskList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="taskList">
+    群发任务列表
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+.taskList {
+  font-size: 20px;
+  color: #00f;
+}
+</style>
--- src/renderer/src/views/groupHairAssistant/index.vue
+++ src/renderer/src/views/groupHairAssistant/index.vue
@@ -1,15 +1,77 @@
 <template>
-  <div class="backgroundWhite">
-    <div class="white">
-      群发助手
+  <div class="groupHairAssistant">
+    <div class="tabsBackground">
+      <el-tabs
+        v-model="activeName"
+        class="demo-tabs"
+        @tab-click="handleClick"
+      >
+        <el-tab-pane
+          label="群发列表"
+          name="massMailingList"
+        />
+        <el-tab-pane
+          label="群发任务列表"
+          name="taskList"
+        />
+      </el-tabs>
+    </div>
+
+    <div class="backgroundWhite">
+      <div class="white">
+        <mass-mailing-list v-show="activeName === 'massMailingList'" />
+        <task-list v-show="activeName === 'taskList'" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import  massMailingList from './components/massMailingList.vue'
+import  taskList from './components/taskList.vue'
+
+const activeName = ref('massMailingList')
+console.log(activeName);
 
 </script>
 
 <style lang="scss" scoped>
+.groupHairAssistant {
+  height: 100%;
+  background-color: #F7F8FA;
+  .tabsBackground {
+    width: 100%;
+    height: 65px;
+    background-color: #fff;
+    padding: 12px 20px;
+    :deep(.el-tabs__item) {
+      font-family: PingFang SC, PingFang SC;
+      font-weight: 500;
+      font-size: 16px;
+      color: #909194;
+      text-align: left;
+      font-style: normal;
+      text-transform: none;
+    }
+    :deep(.el-tabs__nav-wrap::after) {
+      background-color: transparent !important;
+    }
+    :deep(.el-tabs__item.is-active) {
+      font-family: PingFang SC, PingFang SC;
+      font-weight: 600;
+      font-size: 16px;
+      color: #1966FF;
+      text-align: left;
+      font-style: normal;
+      text-transform: none;
+    }
+    :deep(.el-tabs__active-bar) {
+      height: 4px;
+      background: #1966FF;
+      border-radius: 6px;
+    }
+  }
+}
 
 </style>
